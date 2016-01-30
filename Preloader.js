32d100cd634b6552728c8433dfcb6de19276ef30
polferrando98 <pol.ferrando.98@gmail.com>
2016-01-30 01:11:22 +0100

    Pre loader updated

--- Preloader.js
+++ Preloader.js
@@ -35,7 +35,11 @@ BasicGame.Preloader.prototype = {
 	    this.load.image('gorra2', 'res/gorra2.png');
 	    this.load.image('pentinatA1', 'res/pentinatA1.png');
 	    this.load.image('pentinatA2', 'res/pentinatA2.png');
-	    //this.load.image('pentinatA3', 'res/pentinatA3.png');
+	    this.load.image('pentinatA3', 'res/pentinatA3.png');
+	    this.load.image('tamagochi1', 'res/tamagochi1.png');
+	    this.load.image('tamagochi2', 'res/tamagochi2.png');
+	    this.load.image('tamagochi3', 'res/tamagochi3.png');
+	    this.load.image('tamagochiBase', 'res/tamagochiBase.png');
 	    //this.load.image('starfield', 'assets/skies/deep-space.jpg');
 
 	},
